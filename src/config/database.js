a33d1f70d173ccd86fab31b5ed2db305afaf9328
Matheus Rodrigues <joaoomatheus@hotmail.com>
2023-03-14 16:56:23 -0300

    perf: Splitted database to test and to development

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize');
 const is_dev = process.env.NODE_ENV !== 'production';
+const config = require('config');
+
+const db_config = config.get('database');
 
 if (is_dev) {
-  const db_user = process.env.DB_USER;
-  const db_pass = process.env.DB_PASS;
-  const sequelize = new Sequelize('stupid_sn', db_user, db_pass, {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging: false,
+  const sequelize = new Sequelize(db_config.database, db_config.username, db_config.password, {
+    dialect: db_config.dialect,
+    storage: db_config.storage,
+    logging: db_config.logging,
   });
 
   module.exports = sequelize;
